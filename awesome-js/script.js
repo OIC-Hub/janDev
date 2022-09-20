40c93d548b023d28e11c460984b8871c7b557cd5
Sunday Omolewu <omolewu12@gmail.com>
2022-09-20 09:12:11 -0700

    Adding data type

---
+++ awesome-js/script.js
@@ -0,0 +1,21 @@
+ // Variable classs
+ var lastName123, nu, a, b, y;
+ lastName123 = "Omolewu";
+ let firstName = "Sunday";
+
+ let num = 234; 
+ console.log(firstName);
+ console.log(lastName123);
+ console.log(num);
+
+ // Data Type
+ /*
+ 1. String 
+ 2. Number
+ 3. Boolean (True and False)
+ 4. Array  let names = ['Sunday', 'Omolewu', 'Opeyemi', 'Halima', 'Tunmbi', 67];
+  names[2]
+ 5. Object let users = {name:'Sunday Opeyemi', age:23, adress: 'Osogbo'}
+  users['address'];
+  6. undefined 
+ */
