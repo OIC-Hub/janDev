106602dbfee7fc870f4d2de8c8ba98649d235d08
Sunday Omolewu <omolewu12@gmail.com>
2022-09-21 08:31:32 -0700

    Arithematic and assignment operators

--- awesome-js/script.js
+++ awesome-js/script.js
@@ -18,4 +18,44 @@
  5. Object let users = {name:'Sunday Opeyemi', age:23, adress: 'Osogbo'}
   users['address'];
   6. undefined 
+
+
+  |Javascript Operators
+  Arthmetic Operators
+   +
+   -
+   /
+   %
+   *
+   **
+   Assignment Operators
+   let add = "Sunday"
+   add = add + "Omolewu";
+   add += "Omolewu";
+   = 
+   +=
+   *=
+   **=
+   %=
+   /=
+  
+
  */
+let firsName = "Sunday";
+let LastName = "Opeyemi";
+let fullName = "Welcome to our office " + firsName + " " + LastName;
+12 + 45 + "Ade" + 23 + 100;
+let add = 2 * "3";
+let  age;
+age=34;
+age ="Adeojo";
+age=100;
+
+let sum = "OIC ";
+ sum += "Hub ";
+ sum += "Nigeria";
+
+document.getElementById('operator').innerHTML=fullName;
+document.getElementById('suboperator').innerHTML="Assignment concatenation Operator";
+document.getElementById('ans').innerHTML="<code>let Sum=\"OIC\"</code> <br/> <code> Sum +=\"Hub\"</code><br/> <code> Sum +=\"Nigeria\"</code><br/>" +  sum;
+ 
